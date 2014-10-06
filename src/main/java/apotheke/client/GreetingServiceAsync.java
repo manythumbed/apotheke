9b49c718f09785cd5d89b192ca0ead4f0b2e291a
Tim Clark <t.i.j.clark@gmail.com>
2014-10-06 16:12:14 +0100

    Remove comments and tidy

--- src/main/java/apotheke/client/GreetingServiceAsync.java
+++ src/main/java/apotheke/client/GreetingServiceAsync.java
@@ -2,7 +2,6 @@ package apotheke.client;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
-
 public interface GreetingServiceAsync {
 	void greetServer(String input, AsyncCallback<String> callback) throws IllegalArgumentException;
 }
